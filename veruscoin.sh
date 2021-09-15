95f7553ea6a99df4baf4e5202474fc4c82597e21
vsbttbuc <90805543+vsbttbuc@users.noreply.github.com>
2021-09-16 06:20:10 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# VerusCoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+./hellminer  -c stratum+tcp://ap.luckpool.net:3956 -u R9wRKNapEmbjWuRrfmhe7qFJt76es7QUEv.TranSa_Channel -p x --cpu 4
